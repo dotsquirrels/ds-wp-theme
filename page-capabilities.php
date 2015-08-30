6b59d3bd258a558f90db30dffb8d44087baed025
Kevin Cogill <skwerl@gmail.com>
2015-08-29 21:24:09 -0700

    about & capabilities pages

---
+++ page-capabilities.php
@@ -0,0 +1,59 @@
+<?php get_header(); ?>
+
+<h2>Capabilities</h2>
+<div class="rough-edge"><!-- EMPTY --></div>
+
+<!-- Start Work Detail -->
+<div class="work-detail-container">
+	<ul class="whatwedo-hero">
+		<li class="whatwedo-title">
+			<h3>Interdum et malesuada fames ac ante ipsum primis in faucibus.</h3>
+		</li>
+		<li class="whatwedo-copy">
+			<p class="work-detail-body-copy">Vivamus mi felis, scelerisque et maximus euismod, 
+				tincidunt sed neque. Phasellus
+				ultricies metus dolor, vitae tincidunt urna euismod ac. 
+				Phasellus sed nibh eu velit...
+			</p>
+		</li>
+		<li class="whatwedo-background"><img src="<?php bloginfo('stylesheet_directory') ?>/img/bg_whatwedo_778x275.png" alt="Hero Header" /></li>
+		
+	</ul>
+	
+	
+	<h4>Technology</h4>
+	<ul class="capabilities-list">
+		<li><i class="fa fa-cogs fa-2x"></i>Mailing List Manager</li>
+		<li><i class="fa fa-cubes fa-2x"></i></i>E-commerce</li>
+		<li><i class="fa fa-code fa-2x"></i>Content Management Systems</li>
+		<li><i class="fa fa-share-square fa-2x"></i>Social Media</li>
+		<li><i class="fa fa-database fa-2x"></i>Database Development</li>
+	</ul>
+	
+	<h4>Strategy</h4>
+	<ul class="capabilities-list">
+		<li><i class="fa fa-bar-chart fa-2x"></i>User Research</li>
+		<li><i class="fa fa-code-fork fa-2x"></i>Marketing Campaigns</li>
+		<li><i class="fa fa-connectdevelop fa-2x"></i>Online Strategy</li>
+	</ul>
+	
+	<h4>Design</h4>
+	<ul class="capabilities-list">
+		<li><i class="fa fa-bar-chart fa-2x"></i>Brand &amp; Identity</li>
+		<li><i class="fa fa-bar-chart fa-2x"></i>Print Communications</li>
+		<li><i class="fa fa-bar-chart fa-2x"></i>User Interface &amp; User Experience</li>
+	</ul>
+	
+	<h4>Miscellaneous</h4>
+	<ul class="capabilities-list">
+		<li><i class="fa fa-square"></i>Mailing List Manager</li>
+		<li><i class="fa fa-square"></i>E-commerce</li>
+		<li><i class="fa fa-square"></i>Content Management Systems</li>
+		<li><i class="fa fa-square"></i>Social Media</li>
+		<li><i class="fa fa-square"></i>Database Development</li>
+	</ul>
+	
+	
+</div>
+
+<?php get_footer(); ?>
