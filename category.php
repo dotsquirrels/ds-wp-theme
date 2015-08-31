837b487607bf04ff8301c42894bd8eb38f078f95
Skwerl <skwerl@gmail.com>
2015-08-31 00:55:58 -0700

    category page

--- category.php
+++ category.php
@@ -3,65 +3,38 @@
 <h2>Tips & Rants</h2>
 <div class="rough-edge"></div>
 
-<!-- Start Work Detail -->
 <div class="blog-container">
-	
-	<div class="basic-content">
-		<div class="basic-content-hero">
-			<a href="/ds2015/blog/sample-post/"><img class="play-video-button" src="<?php bloginfo('stylesheet_directory') ?>/img/VideoPlayButton_193x193.png" alt="Play Video Button" />
-			<img class="basic-hero" src="<?php bloginfo('stylesheet_directory') ?>/img/VideoImg_778x482.jpg" alt="Hero Header" /></a>
+
+	<?php if (have_posts()) { while (have_posts()) {
+
+		the_post(); 
+
+		?><div class="basic-content">
+			<div class="basic-content-hero">
+				<a href="<?php the_permalink(); ?>"><img class="basic-hero" src="<?php bloginfo('stylesheet_directory') ?>/img/VideoImg_778x482.jpg" alt="Hero Header" /></a>
+			</div>
+			<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+			<p class="basic-content-excerpt"><?php echo get_the_excerpt(); ?></p>
+			<p class="basic-content-date"><?php the_date('F j, Y'); ?></p> 
 		</div>
-		<h3><a href="/ds2015/blog/sample-post/">Interdum et malesuada fames ac ante ipsum primis in faucibus.</a></h3>
-		<p class="work-detail-body-copy">Vivamus mi felis, scelerisque et maximus euismod, tincidunt sed neque. Phasellus
-		ultricies metus dolor, vitae tincidunt urna euismod ac. Phasellus sed nibh eu velit...
-		</p>
-		<p class="basic-content-date">July 30, 2015</p> 
-	</div>
+
+		<?php
 	
-	<ul class="work-detail-tags">
-		<li><p><a href="javascript:void(0);">MOBILE</a><i class="fa fa-tag"></i></p></li>
-		<li><p><a href="javascript:void(0);">E-COMMERCE</a><i class="fa fa-tag"></i></p></li>
-		<li><p><a href="javascript:void(0);">SITE ANALYTICS</a><i class="fa fa-tag"></i></p></li>
-	</ul>
+		$post_tags = get_the_tags();
+		if ($post_tags) {
+			echo '<ul class="work-detail-tags">';
+			foreach($post_tags as $tag) {
+				echo '<li><p><a href="'.get_tag_link($tag->term_id).'">'.$tag->name.'</a><i class="fa fa-tag"></i></p></li>';
+			}
+			echo '</ul>';
+		}		
 	
-	<hr>
-	
-	<div class="basic-content">
-		<div class="basic-content-hero">
-			<a href="/ds2015/blog/sample-post/"><img class="basic-hero" src="<?php bloginfo('stylesheet_directory') ?>/img/blog-img.jpg" alt="Hero Header" /></a>
-		</div>
-		<h3><a href="/ds2015/blog/sample-post/">Interdum et malesuada fames ac ante ipsum primis in faucibus.</a></h3>
-		<p class="work-detail-body-copy">Vivamus mi felis, scelerisque et maximus euismod, tincidunt sed neque. Phasellus
-		ultricies metus dolor, vitae tincidunt...
-		</p>
-		<p class="basic-content-date">July 30, 2015</p> 
-	</div>
-	
-	<ul class="work-detail-tags">
-		<li><p><a href="javascript:void(0);">MOBILE</a><i class="fa fa-tag"></i></p></li>
-		<li><p><a href="javascript:void(0);">E-COMMERCE</a><i class="fa fa-tag"></i></p></li>
-		<li><p><a href="javascript:void(0);">SITE ANALYTICS</a><i class="fa fa-tag"></i></p></li>
-	</ul>
-	
-	<hr>
-	
-	<div class="basic-content">
-		<div class="basic-content-hero">
-			<!--Empty-->
-		</div>
-		<h3><a href="/ds2015/blog/sample-post/">Interdum et malesuada fames ac ante ipsum primis in faucibus.</a></h3>
-		<p class="work-detail-body-copy">Vivamus mi felis, scelerisque et maximus euismod, tincidunt sed neque. Phasellus
-		ultricies metus dolor, vitae tincidunt...
-		</p>
-		<p class="basic-content-date">July 30, 2015</p> 
-	</div>
-	
-	<ul class="work-detail-tags">
-		<li><p><a href="javascript:void(0);">MOBILE</a><i class="fa fa-tag"></i></p></li>
-		<li><p><a href="javascript:void(0);">E-COMMERCE</a><i class="fa fa-tag"></i></p></li>
-		<li><p><a href="javascript:void(0);">SITE ANALYTICS</a><i class="fa fa-tag"></i></p></li>
-	</ul>
-	
+		?>
+
+		<hr><?php
+
+	}} ?>
+
 	<ul class="blog-content-navigation">
 		<li class="older-posts"><a href="javascript:void(0);"><i class="fa fa-caret-left"></i>OLDER</a></li>
 		<li><a href="javascript:void(0);">1</a></li>
