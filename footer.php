86e505479a58f3360d304b99009ccb3cbdfa51bb
Skwerl <skwerl@gmail.com>
2015-09-08 20:52:19 -0700

    responsive tweaks

--- footer.php
+++ footer.php
@@ -11,7 +11,7 @@
 			<div class="contact-details"><a href="tel:+13105662800">310 566 2800</a></div>
 			<div class="contact-details"><a href="mailto:help@dotsquirrels.com">help@dotsquirrels.com</a></div>
 			<hr>
-			<div class="contact-details"><a href="https://www.google.com/maps/place/8731+Venice+Blvd,+Los+Angeles,+CA+90034/@34.032497,-118.383445,17z/" target="_blank">8731 Venice Blvd., Los Angeles, CA 90034</a></div>
+			<div class="contact-details"><a href="https://www.google.com/maps/place/Dot+Squirrels/@34.032497,-118.383445,17z/data=!3m1!4b1!4m2!3m1!1s0x80c2b9829ad2a8e7:0x8d0d1b4b96b7267e" target="_blank">8731 Venice Blvd., Los&nbsp;Angeles,&nbsp;CA&nbsp;90034</a></div>
 				
 			<div class="back-to-top">
 				<a href="#top">Back to top</a>
